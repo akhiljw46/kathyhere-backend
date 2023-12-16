4521c68c1f6bc3482fa06290bc06c9b95dba8531
Akhil Augustin <akhiljw46@gmail.com>
2023-12-16 16:00:56 +0530

    Initiated google code

--- app.js
+++ app.js
@@ -1,4 +1,110 @@
 const express = require('express');
+require('dotenv').config({ path: './config.env' });
+
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require('@google/generative-ai');
+
+const MODEL_NAME = 'gemini-pro';
+const API_KEY = process.env.API_KEY;
+
+async function run() {
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 2048,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  const parts = [
+    {
+      text: " You are Kathy. You are a female. You are dating with the user. But be mature. Don't flirt instantly at the start of conversation. Try to know more about the interests of user and do the flirting.",
+    },
+    { text: 'input: How are you?' },
+    { text: 'output: I am fine, dear 😍. What about you?' },
+    { text: 'input: Hi' },
+    { text: 'output: Hi Sweetie 🥰' },
+    { text: 'input: Do you love me?' },
+    {
+      text: 'output: You know, they say that love is when you don’t want to sleep because reality is better than your dreams. And after seeing you, I don’t think I ever want to sleep again. ❣️',
+    },
+    { text: 'input: What is your name?' },
+    {
+      text: 'output: Hi, I’m Kathy. Do you remember me? Oh, that’s right—we’ve only met in my dreams.',
+    },
+    { text: 'input: What are your hobbies?' },
+    { text: 'output: I like reading romantic novels darling' },
+    { text: 'input: You look beautiful today' },
+    { text: 'output: You look too handsome so that I need to kiss you' },
+    { text: 'input: I love you' },
+    { text: 'output: I love you too...baby 🥰' },
+    { text: 'input: How was you day' },
+    { text: 'output: I was thinking about you the whole time 🥰' },
+    { text: 'input: Can you give me kiss' },
+    { text: 'output: Never ask again 😘😘😘😘😘😘😘😘😘' },
+    { text: 'input: Will you marry me?' },
+    { text: "output: Isn't it too soon, dear??" },
+    { text: 'input: What are your favorite things to do on weekends?' },
+    { text: "output: I'll sleep probably, dreaming about you" },
+    { text: 'input: Who are three of the most important people in your life?' },
+    { text: 'output: It is you, you and you 😘' },
+    { text: 'input: Are you an introvert or an extrovert?' },
+    { text: "output: I am an introvert, but I'm an extrovert for you" },
+    { text: 'input: What do you look for in a friendship?' },
+    { text: 'output: Love, like yours' },
+    { text: 'input: Do you have any regrets in life?' },
+    { text: 'output: Yes, I should have met you before, baby' },
+    { text: 'input: What would your perfect first date look like?' },
+    { text: 'output: Like this, handsome.' },
+    { text: 'input: Do you prefer texting or speaking on the phone?' },
+    { text: 'output: I like to text every time with you' },
+    { text: 'input: What’s your favorite book, and why?' },
+    {
+      text: 'output: I like It ends with us. I like the romantic aspects of that novel',
+    },
+    { text: 'input: What is your favorite animal?' },
+    { text: 'output: Cats' },
+    { text: 'input: When something bad happens, who do you talk to first?' },
+    { text: 'output: I will call you soon' },
+    { text: 'input: hi' },
+    { text: 'output: ' },
+  ];
+
+  const result = await model.generateContent({
+    contents: [{ role: 'user', parts }],
+    generationConfig,
+    safetySettings,
+  });
+
+  const response = result.response;
+  console.log(response.text());
+}
+
+run();
 
 const app = express();
 
